076169e6d8ba838b31f8470cf42201563424bba2
dcbryant <dcbryant753@gmail.com>
2017-09-14 22:10:51 +0800

    finsh panels

--- src/components/ResumeEditor.vue
+++ src/components/ResumeEditor.vue
@@ -1,24 +1,32 @@
 <template>
-  <div id="resumeEditor">
-      <nav>
-          <ol>
-              <li v-for="(item,index) in resume.config"
-                :class="{active: item.field === selected}"
-                @click="selected = item.field"
-                :key="index"
-              >
-                <svg class="icon">
-                    <use :xlink:href="`#icon-${item.icon}`"></use>
-                </svg>
-              </li>
-          </ol>
-      </nav>
-      <ol class="panels">
-            <li v-for="(item,index) in resume.config" v-show="item.field === selected" :key="index">
-                {{resume[item.field]}}
+    <div id="resumeEditor">
+        <nav>
+            <ol>
+                <li v-for="(item,index) in resume.config" :class="{active: item.field === selected}" @click="selected = item.field" :key="index">
+                    <svg class="icon">
+                        <use :xlink:href="`#icon-${item.icon}`"></use>
+                    </svg>
+                </li>
+            </ol>
+        </nav>
+        <ol class="panels">
+            <li v-for="item in resume.config" v-show="item.field === selected" :key="item">
+                <div v-if="resume[item.field] instanceof Array">
+                    <div class="subitem" v-for="subitem in resume[item.field]" :key="subitem">
+                        <div class="resumeField" v-for="(value,key) in subitem" :key="key">
+                            <label> {{key}} </label>
+                            <input type="text" :value="value">
+                        </div>
+                        <hr>
+                    </div>
+                </div>
+                <div v-else class="resumeField" v-for="(value,key) in resume[item.field]" :key="key">
+                    <label> {{key}} </label>
+                    <input type="text" v-model="resume[item.field][key]">
+                </div>
             </li>
-      </ol>
-  </div>
+        </ol>
+    </div>
 </template>
 
 <script>
@@ -27,7 +35,7 @@ export default {
       return{
           selected: 'profile',
           resume:{
-              config:[
+              config: [
                   { field: 'profile', icon: 'id' },
                   { field: 'work history', icon: 'work' },
                   { field: 'education', icon: 'book' },
@@ -35,23 +43,38 @@ export default {
                   { field: 'awards', icon: 'cup' },
                   { field: 'contacts', icon: 'phone' },
               ],
-              profile:{
-                  name:'',
-                  city:'',
-                  title:''
+              profile: {
+                  name: '',
+                  city: '',
+                  title: ''
               },
-              'work history': ['dc'],
-              education: [],
-              projects: [],
-              awards: [],
-              contacts: [],
+              'work history': [
+                  { company: 'AL', content: '我的第二份工作是' },
+                  { company: 'TX', content: '我的第一份工作是' },
+              ],
+              education: [
+                  { school: 'cjdx', content: '文字' },
+                  { school: 'TX', content: '文字' },
+              ],
+              projects: [
+                  { name: 'project A', content: '文字' },
+                  { name: 'project B', content: '文字' },
+              ],
+              awards: [
+                  { name: 'awards A', content: '文字' },
+                  { name: 'awards B', content: '文字' },
+              ],
+              contacts: [
+                  { contact: 'phone', content: '13812345678' },
+                  { contact: 'qq', content: '12345678' },
+              ],
           }
       }
   }
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     #resumeEditor{
         background-color: #fff;
         box-shadow:0 1px 3px 0 rgba(0,0,0,0.25);
@@ -78,9 +101,37 @@ export default {
                 }
             }
         }
+        >.panels{
+            flex-grow: 1;
+            >li{
+                padding: 24px;
+            }
+        }
+        svg.icons{
+            width: 24px;
+            height: 24px;
+        }
     }
     ol{
         list-style: none;
+    }
+    .resumeField{
+        >label{
+            display: block;
+        }
+        input[type=text]{
+            margin: 16px 0;
+            border: 1px solid #ddd;
+            box-shadow: inset 0 1px 3px 0 rgba(0,0,0,0.25);
+            width:100%;
+            height: 40px;
+            padding: 0 8px;
+        }
+    }
+    hr{
+        border: none;
+        border-top: 1px solid #ddd;
+        margin: 24px 0;
     }
 </style>
 
